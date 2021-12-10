8b7cb3b33d60df30562677c1ea4884e1d94d5fcb
choeyeongbi <cyeongb@gmail.com>
2021-12-10 14:58:21 +0900

    add hamburger menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,11 @@
-console.log("test");
+const btnHamburger = document.querySelector("#btnHamburger");
+
+btnHamburger.addEventListener("click", function () {
+  console.log("hamburger menu click");
+
+  if (btnHamburger.classList.contains("open")) {
+    btnHamburger.classList.remove("open");
+  } else {
+    btnHamburger.classList.add("open");
+  }
+});
