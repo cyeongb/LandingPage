5dc9f524fca9d414648669fdc0d4a6ad7fb02a36
choeyeongbi <cyeongb@gmail.com>
2021-12-11 15:54:27 +0900

    mobile menu layout complete

--- app/js/script.js
+++ app/js/script.js
@@ -1,19 +1,29 @@
+const body = document.querySelector("body");
+
 const btnHamburger = document.querySelector("#btnHamburger");
 
 const header = document.querySelector(".header");
 
 const overlay = document.querySelector(".overlay");
 
+const fadeEls = document.querySelectorAll(".has-fade");
+
 btnHamburger.addEventListener("click", function () {
   if (header.classList.contains("open")) {
     //close menu toggle
+    body.classList.remove("noscroll");
     header.classList.remove("open");
-    overlay.classList.remove("fade-in");
-    overlay.classList.add("fade-out");
+    fadeEls.forEach(function (el) {
+      el.classList.remove("fade-in");
+      el.classList.add("fade-out");
+    });
   } else {
     //open menu toggle
+    body.classList.add("noscroll"); // 메뉴열 때 스크롤 안되게
     header.classList.add("open");
-    overlay.classList.remove("fade-out");
-    overlay.classList.add("fade-in");
+    fadeEls.forEach(function (el) {
+      el.classList.remove("fade-out");
+      el.classList.add("fade-in");
+    });
   }
 });
